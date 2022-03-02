9908214ca4a78d05c173421041c6b932fc6348db
Rongbin Li <lirongbin.john@gmail.com>
2022-03-02 10:21:58 -0800

    1024 model try

--- HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
+++ HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
@@ -41,15 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
       Log.i(TAG, String.format("The shape of orginal bitmap (wxh): %dx%d", orgBitmap.getWidth(), orgBitmap.getHeight()));
 
-      final int inputWidth = 256;
-      final int inputHeight = 256;
+      final int inputWidth = 1024;
+      final int inputHeight = 1024;
       bitmap = Bitmap.createScaledBitmap(orgBitmap, inputWidth, inputHeight, true);
 
       Log.i(TAG, String.format("The shape of resized bitmap (wxh): %dx%d", bitmap.getWidth(), bitmap.getHeight()));
 
       // loading serialized torchscript module from packaged into app android asset model.pt,
       // app/src/model/assets/model.pt
-      module = LiteModuleLoader.load(assetFilePath(this, "StyleGAN_converter_LeakyReLU_02222022.ptl"));
+      module = LiteModuleLoader.load(assetFilePath(this, "StyleGAN_1024_random_02222022.ptl"));
     } catch (IOException e) {
       Log.e("PytorchHelloWorld", "Error reading assets", e);
       finish();
