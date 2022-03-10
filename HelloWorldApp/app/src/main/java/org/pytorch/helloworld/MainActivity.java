a2ed1de64134eb61fe8e21990d9f2afc76ff4d20
Rongbin Li <lirongbin.john@gmail.com>
2022-03-09 16:08:23 -0800

    Support both classification model and gan model.

--- HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
+++ HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
@@ -8,9 +8,11 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.pytorch.Device;
 import org.pytorch.IValue;
 import org.pytorch.LiteModuleLoader;
 import org.pytorch.Module;
+import org.pytorch.PyTorchAndroid;
 import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 import org.pytorch.MemoryFormat;
@@ -27,6 +29,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "oua_" + MainActivity.class.getSimpleName();
 
+  private static final boolean CLASSIFICATION_MODEL = true;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -39,19 +43,37 @@ public class MainActivity extends AppCompatActivity {
       // app/src/main/assets/image.jpg
       Bitmap orgBitmap = BitmapFactory.decodeStream(getAssets().open("image.jpg"));
 
-      Log.i(TAG, String.format("The shape of orginal bitmap (wxh): %dx%d", orgBitmap.getWidth(), orgBitmap.getHeight()));
+      Log.i(TAG, String.format("The shape of original bitmap (wxh): %dx%d", orgBitmap.getWidth(), orgBitmap.getHeight()));
 
-      final int inputWidth = 1024;
-      final int inputHeight = 1024;
-      bitmap = Bitmap.createScaledBitmap(orgBitmap, inputWidth, inputHeight, true);
+      if (CLASSIFICATION_MODEL) {
+        Log.i(TAG, "Classification model");
+        bitmap = orgBitmap;
+      } else {
+        final int inputWidth = 256;
+        final int inputHeight = 256;
+        bitmap = Bitmap.createScaledBitmap(orgBitmap, inputWidth, inputHeight, true);
+      }
 
       Log.i(TAG, String.format("The shape of resized bitmap (wxh): %dx%d", bitmap.getWidth(), bitmap.getHeight()));
 
+      // Seems that the default numThreads are not 1, because, by experiments, default number
+      // works almost as good as numThreads == 4 (which is best by my experiments).
+      // So, I guess pytorch andriod use a good default number.
+      PyTorchAndroid.setNumThreads(4);
+
       // loading serialized torchscript module from packaged into app android asset model.pt,
       // app/src/model/assets/model.pt
-      module = LiteModuleLoader.load(assetFilePath(this, "StyleGAN_1024_random_02222022.ptl"));
+      module = LiteModuleLoader.load(
+              assetFilePath(this, "mobilenet_v2.pt"),
+              null,
+              Device.CPU);
+      Log.i(TAG, "After load model");
     } catch (IOException e) {
-      Log.e("PytorchHelloWorld", "Error reading assets", e);
+      Log.e(TAG, "Error reading assets", e);
+      finish();
+    } catch (Exception e) {
+      Log.e(TAG, "Unexpected exception");
+      Log.e(TAG, e.getMessage(), e);
       finish();
     }
 
@@ -66,7 +88,10 @@ public class MainActivity extends AppCompatActivity {
     Log.i(TAG, "input_shape=" + Arrays.toString(inputTensor.shape()));
 
     // running the model
-    final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+    Tensor outputTensor = null;
+    for (int k=0; k<50; k++) {
+      outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+    }
 
     Log.i(TAG, "inference done");
 
@@ -74,22 +99,28 @@ public class MainActivity extends AppCompatActivity {
     final float[] output = outputTensor.getDataAsFloatArray();
     Log.i(TAG, "output: " + Arrays.toString(output));
 
+    if (!CLASSIFICATION_MODEL) {
+      ((TextView) findViewById(R.id.text)).setText("Done");
+      return;
+    }
+
+    final float[] scores = output;
+
     // searching for the index with maximum score
-//    float maxScore = -Float.MAX_VALUE;
-//    int maxScoreIdx = -1;
-//    for (int i = 0; i < scores.length; i++) {
-//      if (scores[i] > maxScore) {
-//        maxScore = scores[i];
-//        maxScoreIdx = i;
-//      }
-//    }
-//
-//    String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
+    float maxScore = -Float.MAX_VALUE;
+    int maxScoreIdx = -1;
+    for (int i = 0; i < scores.length; i++) {
+      if (scores[i] > maxScore) {
+        maxScore = scores[i];
+        maxScoreIdx = i;
+      }
+    }
+
+    String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
 
     // showing className on UI
     TextView textView = findViewById(R.id.text);
-//    textView.setText(className);
-      textView.setText("Done");
+    textView.setText(className);
   }
 
   /**
